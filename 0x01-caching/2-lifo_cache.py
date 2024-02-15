ec2493b5e64cc82098f440aea0fc6fb262b34556
horlami228 <akintolaolamilekan51@gmail.com>
2024-02-15 23:11:06 +0100

    completed the lifo task

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,37 +1,42 @@
 #!/usr/bin/env python3
 
-"""FIFO system"""
+"""LIFO system"""
 from base_caching import BaseCaching
 from threading import RLock
 
 
-class FIFOCache(BaseCaching):
-    """Defines a fifo algorithm"""
+class LIFOCache(BaseCaching):
+    """Defines a LIFO algorithm"""
 
     def __init__(self):
         """Initialize the class"""
         super().__init__()
-        self._keys = []
+        self._stack_keys = []
         self._Rlock = RLock()
 
     def put(self, key, item):
         """Putting item in the dictionary"""
-
         with self._Rlock:
             if key is None or item is None:
                 return
-
             if key in self.cache_data:
                 self.cache_data[key] = item
 
+                if key in self._stack_keys:
+                    ''''move key to the top of the stack'''
+                    self._stack_keys.remove(key)
+                    self._stack_keys.append(key)
+
             else:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    deleted_key = self._keys.pop(0)
-                    self.cache_data.pop(deleted_key)
-                    print(f"DISCARD: {deleted_key}")
+
+                    """discard the top key"""
+                    discarded_key = self._stack_keys.pop()
+                    del self.cache_data[discarded_key]
+                    print(f"DISCARD: {discarded_key}")
 
                 self.cache_data[key] = item
-                self._keys.append(key)
+                self._stack_keys.append(key)
 
     def get(self, key):
         """
